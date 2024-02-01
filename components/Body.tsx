b6a9ed5fbe8890d9877f1c97e6a3659312f1061c
Валера <i.radetskyi@batyevka.net>
2024-02-01 14:33:10 +0200

    First variant of proping. Fallback to v1 component Heading.tsx. The AI addiction, begining

--- components/Body.tsx
+++ components/Body.tsx
@@ -5,15 +5,14 @@ import ContactForm from './ihorradetskyi/ContactForm';
 import InfoBlock from './ihorradetskyi/InfoBlock';
 import Advantages from './ihorradetskyi/Advantages';
 import Slider from './ihorradetskyi/Slider';
+import Heading from './ihorradetskyi/Heading';
 
 const Body = () => {
     return ( 
         <body className={`bg-[#0E2D43]`}>
             <main className={`mx-auto w-[1110px] bg-[#0E2D43]`}>
                 <Slider/>
-                <div className={` text-[42px] font-bold text-white flex items-center justify-center w-[1110px]`}>
-                    <h1 className={`text-center w-[913px] flex items-center justify-center`}>Ключові переваги для наших бізнес абонентів</h1>
-                </div>
+                <Heading text={'Ключові переваги для наших бізнес абонентів'}></Heading>
                 <Advantages/>
                 <Tarifs/>
                 <ContactForm/>
