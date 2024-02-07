5c8b4821ddd8aa277aba796b94e2364aa12240c1
Валера <i.radetskyi@batyevka.net>
2024-02-07 11:09:07 +0200

    Header, Logo reworking

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,7 @@ const Header = () => {
         <div className={`flex justify-between items-center h-[70px] my-[30px] max-[720px]:h-[50px] max-[720px]:my-0`}>
             <nav className={`flex items-center space-x-4 ml-[50px] min-w-max max-[720px]:ml-[20px]`}>
                 <Link href='#'  className={``}>
-                    <Image src={logo} height={1} className={`flex-shrink-0 max-[720px]:w-[158px] min-[2430px]:h-[70px]  min-[3644px]:h-[104 px] min-[2430px]:h-[70px] min-[2430px]:w-[403px] min-[3644px]:h-[104 px]`} alt={'Batyevka logo'}/>
+                    <Image src={logo} height={1} className={`flex-shrink-0 max-[720px]:w-[203px] max-[720px]:h-[35px] min-[2430px]:h-[70px] min-[3644px]:h-[104px] max-[1080px]:w-[308px] max-[1080px]:h-[53px] min-[1080px]:w-[308px] min-[1080px]:h-[53px] min-[2430px]:w-[403px] min-[3644px]:h-[104 px]`} alt={'Batyevka logo'}/>
                 </Link>
                 <Link href='#'  className={`max-[1666px]:hidden`}>
                     <Image src={bandit_ban} className={`min-w-full`} alt={'ArrowDown'}/>
