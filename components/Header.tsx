fbd58c4d0446d343ba7cda9e3f4b844c848e72a8
Валера <i.radetskyi@batyevka.net>
2024-03-11 14:15:23 +0200

    Navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,10 @@ import { useModal } from "@/hooks/use-modal-store";
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../public/img/logo.svg';
-import msg_small from '../public/img/msg_small.svg';
 import arrow_down from '../public/img/arrow_down.svg';
-import ket_small from '../public/img/ket_small.svg';
 import globe_small from '../public/img/globe_small.svg';
-import wallet from '../public/img/wallet.svg';
 import wallet_white from '../public/img/wallet_white.svg';
 import _247 from '../public/img/247.svg';
-import gear from '../public/img/gear.svg';
-import bandit_ban from '../public/img/bandit-ban.svg';
 import telephon from '../public/img/phone.svg';
 import rectangle from '../public/img/rectangle.svg';
 import all_services from '../public/img/all_services.svg';
@@ -22,7 +17,7 @@ import mobLogo from '../public/img/mobLogo.svg'
 import { Button } from '@/components/ui/button';
 import mobButton from '../public/img/mobButton.svg'
 
-
+import Navbar from "./Navbar";
 
 const Header = () => {
 
@@ -111,34 +106,9 @@ const Header = () => {
             <Link href='#'  className={`max-[1420px]:hidden text-white font-semibold text-[13px] min-[2430px]:text-[21px] min-[2430px]:leading-[28px] text-center`}>Відеоспостереження <br/>для бізнесу</Link>
             <Link href='#'  className={`text-white inline-flex font-semibold  text-[13px] min-[2430px]:text-[21px] min-[2430px]:leading-[28px] text-center`}>Всі послуги <Image src={all_services} className={`ml-2`} alt='All Services'/></Link>
         </div>
-        <div className="flex items-center justify-center bg-[#0D2A40]">
-            <div className="min-[780px]:hidden bg-[#0D2A40] h-[60px] flex items-center justify-start w-[300px]">
-                <Image src={mobLogo} alt="Batyevka.NET"></Image>
-                <div className="flex ml-[10px]">
-                <Image src={telephon} className={`w-[20px] h-[20px] mr-[9.75px]`} alt={'phone'}/>
-                <a className='font-semibold text-white text-[24px] leading-[22px]' href={`tel:0800303230`}>0 800 30 32 30</a>
-                </div>
-            </div>
-        </div>
-        <div className="mt-[16px] min-[780px]:hidden flex items-center justify-center">
-            <Button onClick={() => {onOpen("payment")}} variant="payMobMob">
-                <Image src={wallet_white} className={`relative w-7 h-7 fill-white`} alt={'wallet'}></Image>
-            </Button>
-            <Button variant="cabinetMobMob">
-                <Image src={connect} alt='connect' className={``}>
-                </Image>
-            </Button>
-            <Button onClick={() => onOpen("call")} variant="connectMobMob">
-                <Image src={wkey} alt='wkey'>
-                </Image>
-            </Button>
 
-            <Button variant="menuMob">
-                <Image src={mobButton} alt='Menu' className={`h-[60px] w-[63px]`}>
-                </Image>
-            </Button>
+        <Navbar/>
 
-        </div>
         </header>
     );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { useModal } from "@/hooks/use-modal-store";
+
+import Image from 'next/image';
+import wallet_white from '../public/img/wallet_white.svg';
+import _247 from '../public/img/247.svg';
+import telephon from '../public/img/phone.svg';
+import connect from '../public/img/connect.svg';
+import wkey from '../public/img/wkey.svg'
+import mobLogo from '../public/img/mobLogo.svg'
+import { Button } from '@/components/ui/button';
+import mobButton from '../public/img/mobButton.svg'
+
+const Navbar = () => {
+
+    const { onOpen } = useModal();
+
+    
+    return ( 
+        <>
+        <div className="flex items-center justify-center bg-[#0D2A40]">
+            <div className="min-[780px]:hidden bg-[#0D2A40] h-[60px] flex items-center justify-start w-[300px]">
+                <Image src={mobLogo} alt="Batyevka.NET"></Image>
+                <div className="flex ml-[10px]">
+                <Image src={telephon} className={`w-[20px] h-[20px] mr-[9.75px]`} alt={'phone'}/>
+                <a className='font-semibold text-white text-[24px] leading-[22px]' href={`tel:0800303230`}>0 800 30 32 30</a>
+                </div>
+            </div>
+        </div>
+        <div className="mt-[16px] min-[780px]:hidden flex items-center justify-center">
+            <Button onClick={() => {onOpen("payment")}} variant="payMobMob">
+                <Image src={wallet_white} className={`relative w-7 h-7 fill-white`} alt={'wallet'}></Image>
+            </Button>
+            <Button variant="cabinetMobMob">
+                <Image src={connect} alt='connect' className={``}>
+                </Image>
+            </Button>
+            <Button onClick={() => onOpen("call")} variant="connectMobMob">
+                <Image src={wkey} alt='wkey'>
+                </Image>
+            </Button>
+
+            <Button variant="menuMob">
+                <Image src={mobButton} alt='Menu' className={`h-[60px] w-[63px]`}>
+                </Image>
+            </Button>
+        </div>
+        </>
+     );
+}
+ 
+export default Navbar;
