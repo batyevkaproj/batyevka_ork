80232f21bb05c2e5418169d5b29d0920a4a0c08c
Валера <i.radetskyi@batyevka.net>
2024-03-19 00:20:29 +0200

    123

--- components/business-page/Slider.tsx
+++ components/business-page/Slider.tsx
@@ -10,6 +10,16 @@ import vector4 from   '../../public/img/vector4.svg';
 
 import Image from 'next/image';
 
+
+
+const fetchedData = [
+    { "офіс": "../../public/img/Vector.svg" },
+    { "магазин": "../../public/img/vector2.svg" },
+    { "салон": "../../public/img/vector3.svg" },
+    { "ресторан": "../../public/img/vector4.svg" }
+];
+
+
 import {
     Carousel,
     CarouselContent,
@@ -50,8 +60,44 @@ const Slider = () => {
 
 <Carousel className={`h-[446px] mt-[45px]`} setApi={setApi}>
   <CarouselContent className='flex'>
+
+
+  {fetchedData.map((data, index) => (
+    <CarouselItem key={index} className='flex items-center justify-between'>
+        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-1 max-[910px]:hidden'>
+            <h1 className={`text-[60px] font-bold text-white whitespace-nowrap`}>Інтернет <span className={`text-[#56AABF]`}>в {Object.keys(data)[0].toUpperCase()}</span></h1>
+            <li className={`list-none text-white flex flex-col mb-[30px]`}>
+                <div className={`flex items-center mb-[15px]`}>
+                    <span className={`box-border w-5 flex`}>
+                        <Image src={infinity} className={`inline-flex`} alt='infinity'/>
+                    </span>
+                    <ul className={`ml-2 font-normal`}>Безлімітні та необмежені тарифні плани</ul>
+                </div>
+                <div className={`flex items-center mb-[15px]`}>
+                    <span className={`box-border w-[20px] pl-[5px] flex justify-center self-start pt-[5px]`}>
+                        <Image src={sign} className={`inline-flex`} alt={`sign`}/>
+                    </span>
+                    <ul className={`ml-3 font-normal`}>Гарантована швидкість – симетричний високошвидкісний канал із постійною швидкістю доступу до мережі Інтернет</ul>
+                </div>
+                <div className={`flex items-center`}>
+                    <span className={`box-border w-5 flex justify-center self-start`}>
+                        <Image src={cable} className={`inline-flex`} alt={`cable`}/>
+                    </span>
+                    <ul className={`ml-2 font-normal`}>Організація доступу в Інтернет за найкращою технологією (Оптоволокно)</ul>
+                </div>
+            </li>
+            <button className={`bg-[#56AABF] text-white rounded-full h-[60px] w-[155px] cursor-pointer font-semibold shadow-[0_4px_20px_0_#56AABF80]`}>Замовити</button>
+        </div>
+        <div className='mr-[120px] z-10 max-[910px]:pl-[20%] max-[910px]:box-border max-[910px]:whitespace-nowrap max-[910px]:min-w-full'>
+            {/* <Image src={Object.values(data)[0]} alt={`vector`} /> */}
+        </div>
+    </CarouselItem>
+))}
+
+
+
     <CarouselItem className='flex justify-between items-center'>
-        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-10 max-[910px]:hidden'>
+        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-1 max-[910px]:hidden'>
             <h1 className={`text-[60px] font-bold text-white shrink-0 whitespace-nowrap z-10`}>Інтернет <span className={`text-[#56AABF]`}>В офіс</span></h1>
             <li className={`list-none text-white flex flex-col mb-[30px]`}>
                 <div className={`flex items-center mb-[15px]`}>
@@ -78,12 +124,12 @@ const Slider = () => {
             <button className={`bg-[#56AABF] text-white rounded-full h-[60px] w-[155px] cursor-pointer  font-semibold shadow-[0_4px_20px_0_#56AABF80]`}>Замовити</button>
         </div>
         <div className='mr-[120px] z-10 max-[910px]:pl-[20%] max-[910px]:box-border max-[910px]:whitespace-nowrap max-[910px]:min-w-full'>
-            <Image src={vector} className='z-1 whitespace-nowrap' alt='vector'/>
+            <Image src={vector} className='z-10 whitespace-nowrap' alt='vector'/>
         </div>
     </CarouselItem>
     <CarouselItem className='flex items-center justify-between'>
-        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-10 max-[910px]:hidden'>
-            <h1 className={` text-[60px] font-bold text-white whitespace-nowrap`}>Інтернет <span className={`text-[#56AABF]`}>в МАГАЗИН</span></h1>
+        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-1 max-[910px]:hidden'>
+            <h1 className={` text-[60px] font-bold text-white whitespace-nowrap z-1`}>Інтернет <span className={`text-[#56AABF]`}>в МАГАЗИН</span></h1>
             <li className={`list-none text-white flex flex-col mb-[30px]`}>
                 <div className={`flex items-center mb-[15px]`}>
                 <span className={`box-border w-5 flex`}>
@@ -113,7 +159,7 @@ const Slider = () => {
         </div>
     </CarouselItem>
     <CarouselItem className='flex items-center justify-between '>
-        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-10 max-[910px]:hidden'>
+        <div className='ml-[120px] max-[1706px]:w-[655px] max-[1400px]:w-[430px] z-1 max-[910px]:hidden'>
             <h1 className={` text-[60px] font-bold text-white whitespace-nowrap`}>Інтернет <span className={`text-[#56AABF]`}>в САЛОН</span></h1>
             <li className={`list-none text-white flex flex-col mb-[30px]`}>
                 <div className={`flex items-center mb-[15px]`}>
