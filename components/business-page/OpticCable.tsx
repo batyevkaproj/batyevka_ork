9fa4d05c98e9417343ca90e4df1e01a23add26ae
Валера <i.radetskyi@batyevka.net>
2024-03-21 11:18:08 +0200

    f2ef

--- components/business-page/OpticCable.tsx
+++ components/business-page/OpticCable.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 
-import { cn } from "@/lib/utils";
 
 import flag from '../../public/img/prideFlag.svg'
 
 const OpticCable = () => {
     return ( 
-        <div className="flex absolute left-[-60vh] max-[1770px]:hidden">
-            <div className="h-[45px] w-[1500px] bg-[#123853] top-96">
+        <div className="flex absolute">
+            <div className="h-[45px] w-[50vh] bg-[#123853] top-96">
             </div>
             <Image src={flag} className="w-[210px] h-[44px]" alt="DawnPride" />
         </div>
