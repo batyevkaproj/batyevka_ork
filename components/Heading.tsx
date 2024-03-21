83bd07023b228909d5d41a871f77f11b6a539489
Валера <i.radetskyi@batyevka.net>
2024-03-21 14:06:21 +0200

    123

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -5,7 +5,7 @@ type HeadingProps = {
 
 const Heading = ({text}: HeadingProps) => {
     return ( 
-        <div className={`text-[42px] font-bold text-white flex items-center justify-center w-full`}>
+        <div className={`text-[42px] font-bold text-white flex items-center justify-center w-full mx-auto`}>
             <h1 className={`text-center flex items-center justify-center`}>{text}</h1>
         </div>
     );
