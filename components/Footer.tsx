209d81bfd1a14563635f2190bb646b981e37000c
Kolyaska2002 <artsem112@gmail.com>
2024-04-16 22:45:21 +0200

    Basic 2k footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,29 +13,32 @@ import Image from 'next/image';
 const Footer = () => {
     return ( 
         <footer className={`bg-[#0E2D43] shadow-[0_4px_29px_0_rgba(8,35,27)]`}>
-            <div className={`pt-[60px] grid grid-cols-6 text-white gap-4  font-normal leading-[30px]`}>
+            <div className={`pt-[60px] grid grid-cols-6 text-white gap-[43.2px] font-normal leading-[30px] mr-[170px] ml-[170px] pb-[78px]`}>
                 <div>
                     <div>
-                        <h3 className={`border-b border-white border-solid font-semibold pb-[11px] mb-[8px]`}>Ми надаємо послуги:</h3>
-                        <ul>
-                            <li>Швидкісного достуру в Інтернет</li>
+                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Ми надаємо послуги:</h3>
+                        <ul className={'leading-[36px]'}>
+                            <li>Швидкісного доступу в Інтернет</li>
                             <li>Інтерактивне Телебачення</li>
                             <li>ось вже:</li>
-
+                            <div className={'h-[48px] w-[282px] rounded-xl bg-[#123853]'}>
+                                <p className={'font-semibold ml-[17px] pt-[7px]'}>24 роки 5 місяців</p>
+                            </div>
+                            <li className={'text-[#56AABF] font-medium leading-[34px] pt-[111px]'}>© Batyevka 2014-2024</li>
                         </ul>
                     </div>
                 </div>
                 <div>
-                    <h3 className={`border-b border-white border-solid font-semibold pb-[11px] mb-[8px]`}>Тарифи</h3>
-                <ul>
-                    <li>Для Бізнесу</li>
-                    <li>Для Багатоповерхівок</li>
-                    <li>Для Приватних Будинків</li>
-                </ul>
+                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>ТАРИФИ</h3>
+                    <ul className={'leading-[36px]'}>
+                        <li>Для Бізнесу</li>
+                        <li>Для Багатоповерхівок</li>
+                        <li>Для Приватних Будинків</li>
+                    </ul>
                 </div>
                 <div>
-                    <h3 className={`border-b border-white border-solid font-semibold pb-[11px] mb-[8px]`}>Інформація абонентам</h3>
-                    <ul>
+                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Інформація абонентам</h3>
+                    <ul className={'leading-[36px]'}>
                         <li>Публічний договір</li>
                         <li>Мапа покриття</li>
                         <li>Особитстий кабінет</li>
@@ -44,26 +47,26 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <h3 className={`border-b border-white border-solid font-semibold pb-[11px] mb-[8px]`}>Технічна підтримка</h3>
-                    <ul>
-                        <li><Image className='inline mr-[10px]' src={orange_mini_phone} alt={``}/>044 49 555 49</li>
-                        <li><Image className='inline mr-[10px]' src={orange_mini_mail} alt={``}/>support@batyevka.net</li>
-                        <li><Image className='inline mr-[10px]' src={orange_mini_tg_logo} alt={``}/>бот в телеграмм</li>
+                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Технічна підтримка</h3>
+                    <ul className={'leading-[36px]'}>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[21px]' src={orange_mini_phone} alt={``}/>044 49 555 49</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[16px]' src={orange_mini_mail} alt={``}/>support@batyevka.net</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[19px]' src={orange_mini_tg_logo} alt={``}/>бот в телеграмм</li>
                     </ul>
                 </div>
                 <div>
-                    <h3 className={`border-b border-white border-solid font-semibold pb-[11px] mb-[8px]`}>Контакти</h3>
-                    <ul>
-                        <li><Image className='inline mr-[10px]' src={orange_mini_place_marker} alt={``}/>03110, м. Київ, а/с 26</li>
-                        <li><Image className='inline mr-[10px]' src={orange_mini_phone} alt={``}/>0 800 30 32 30</li>
-                        <li><Image className='inline mr-[10px]' src={orange_mini_mail} alt={``}/>dogovor@batyevka.net</li>
+                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Контакти</h3>
+                    <ul className={'leading-[36px]'}>
+                        <li><Image className='inline mr-[10px] w-[16px] h-[21px]' src={orange_mini_place_marker} alt={``}/>03110, м. Київ, а/с 26</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[21px]' src={orange_mini_phone} alt={``}/>0 800 30 32 30</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[16px]' src={orange_mini_mail} alt={``}/>dogovor@batyevka.net</li>
                     </ul>    
                 </div>
                 <div>
-                    <h3 className={`border-b border-white border-solid font-semibold pb-[11px] mb-[8px]`}>Соціальні мережі</h3>
+                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[24px]`}>Соціальні мережі</h3>
                     <div className={`flex`}>
-                        <a href={``}><Image src={_1} alt={``}/></a>
-                        <a href={``}><Image src={_3} alt={``}/></a>
+                        <a href={``}><Image className='w-[40px] h-[40px] mr-[8px]' src={_1} alt={``}/></a>
+                        <a href={``}><Image className='w-[40px] h-[40px]' src={_3} alt={``}/></a>
                     </div>
                 </div>
             </div>
