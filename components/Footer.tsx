108c395d0f91986fdb18bbc7843ab86800392092
Kolyaska2002 <artsem112@gmail.com>
2024-04-17 17:29:08 +0200

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,24 +52,24 @@ const Footer = () => {
                 <div>
                     <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Технічна підтримка</h3>
                     <ul className={'leading-[36px]'}>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[21px]' src={orange_mini_phone} alt={``}/>044 49 555 49</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[16px]' src={orange_mini_mail} alt={``}/>support@batyevka.net</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[19px]' src={orange_mini_tg_logo} alt={``}/>бот в телеграмм</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[21px] max-[1920px]:w-[16px] max-[1920px]:h-[16px]' src={orange_mini_phone} alt={``}/>044 49 555 49</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[16px] max-[1920px]:w-[16px] max-[1920px]:h-[12px]' src={orange_mini_mail} alt={``}/>support@batyevka.net</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[19px] max-[1920px]:w-[17px] max-[1920px]:h-[15px]' src={orange_mini_tg_logo} alt={``}/>бот в телеграмм</li>
                     </ul>
                 </div>
                 <div className={'col-span-1 max-[1024px]:col-start-3 max-[1024px]:row-start-2 max-[1024px]:row-span-1'}>
                     <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Контакти</h3>
                     <ul className={'leading-[36px]'}>
-                        <li><Image className='inline mr-[10px] w-[16px] h-[21px]' src={orange_mini_place_marker} alt={``}/>03110, м. Київ, а/с 26</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[21px]' src={orange_mini_phone} alt={``}/>0 800 30 32 30</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[16px]' src={orange_mini_mail} alt={``}/>dogovor@batyevka.net</li>
+                        <li><Image className='inline mr-[10px] w-[16px] h-[21px] max-[1920px]:w-[12px] max-[1920px]:h-[16px]' src={orange_mini_place_marker} alt={``}/>03110, м. Київ, а/с 26</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[21px] max-[1920px]:w-[16px] max-[1920px]:h-[16px]' src={orange_mini_phone} alt={``}/>0 800 30 32 30</li>
+                        <li><Image className='inline mr-[10px] w-[21px] h-[16px] max-[1920px]:w-[16px] max-[1920px]:h-[12px]' src={orange_mini_mail} alt={``}/>dogovor@batyevka.net</li>
                     </ul>    
                 </div>
                 <div className={'max-[1920px]:col-start-5 max-[1920px]:row-start-2 max-[1920px]:row-span-1 max-[1280px]:col-start-4 max-[1280px]:row-start-2 max-[1280px]:row-span-1 max-[1024px]:col-start-3 max-[1024px]:row-start-3 max-[1024px]:row-span-1'}>
-                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[24px]`}>Соціальні мережі</h3>
+                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[24px] max-[1920px]:mb-[19px]`}>Соціальні мережі</h3>
                     <div className={`flex`}>
-                        <a href={``}><Image className='w-[40px] h-[40px] mr-[8px]' src={_1} alt={``}/></a>
-                        <a href={``}><Image className='w-[40px] h-[40px]' src={_3} alt={``}/></a>
+                        <a href={``}><Image className='w-[40px] h-[40px] max-[1920px]:w-[30px] max-[1920px]:h-[30px] mr-[8px]' src={_1} alt={``}/></a>
+                        <a href={``}><Image className='w-[40px] h-[40px] max-[1920px]:w-[30px] max-[1920px]:h-[30px]' src={_3} alt={``}/></a>
                     </div>
                 </div>
             </div>
