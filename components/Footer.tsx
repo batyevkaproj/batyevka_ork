3c5d328814352febf8f9e290f19081616cbd87cc
Kolyaska2002 <artsem112@gmail.com>
2024-04-18 18:55:12 +0200

    test new accordion before fuck up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,72 +6,211 @@ import orange_mini_phone from '../public/img/orange_mini_phone.svg';
 import orange_mini_place_marker from '../public/img/orange_mini_place_marker.svg';
 import orange_mini_tg_logo from '../public/img/orange_mini_tg_logo.svg';
 import orange_mini_mail from '../public/img/orange_mini_mail.svg';
-
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion_footer";
+import Link from 'next/link';
 
 import Image from 'next/image';
 
 const Footer = () => {
     return ( 
         <footer className={`bg-[#0E2D43] shadow-[0_4px_29px_0_rgba(8,35,27)]`}>
-            <div className={`pt-[60px] grid grid-cols-6 max-[1920px]:grid-cols-5 max-[1280px]:grid-cols-4 max-[1024px]:grid-cols-3 grid-rows-3 text-white text-[18px] max-[1920px]:text-[14px] gap-[43.2px] font-normal leading-[30px] mr-[170px] ml-[170px] pb-[78px] max-[1920px]:mr-[120px] max-[1920px]:ml-[120px]  max-[1280px]:mr-[85px] max-[1280px]:ml-[85px] max-[1024px]:mr-[67px] max-[1024px]:ml-[67px]`}>
-                <div className={'col-span-1 row-span-2'}>
+            <div className={'max-[800px]:hidden'}>
+                <div className={`pt-[60px] grid grid-cols-6 max-[1920px]:grid-cols-5 max-[1280px]:grid-cols-4 max-[1024px]:grid-cols-3 grid-rows-3 text-white text-[18px] max-[1920px]:text-[14px] gap-[43.2px] font-normal leading-[30px] mr-[170px] ml-[170px] pb-[78px] max-[1920px]:mr-[120px] max-[1920px]:ml-[120px]  max-[1280px]:mr-[85px] max-[1280px]:ml-[85px] max-[1024px]:mr-[67px] max-[1024px]:ml-[67px]`}>
+                    <div className={'col-span-1 row-span-2'}>
+                        <div>
+                            <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Ми надаємо послуги:</h3>
+                            <ul className={'leading-[36px]'}>
+                                <li>Швидкісного доступу в Інтернет</li>
+                                <li>Інтерактивне Телебачення</li>
+                                <li>ось вже:</li>
+                                <div className={'h-[48px] max-w-[282px] max-[1920px]:max-w-[216px] min-w-[216px] rounded-xl bg-[#123853]'}>
+                                    <p className={'font-semibold ml-[17px] pt-[7px]'}>24 роки 5 місяців</p>
+                                </div>
+                            </ul>
+                        </div>
+                        
+                    </div>
+                    <div className={'col-span-1 row-start-3'}>
+                            <a className={'text-[#56AABF] font-semibold leading-[34px]'}>© Batyevka 2014-2024</a>
+                    </div>
                     <div>
-                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Ми надаємо послуги:</h3>
+                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>ТАРИФИ</h3>
                         <ul className={'leading-[36px]'}>
-                            <li>Швидкісного доступу в Інтернет</li>
-                            <li>Інтерактивне Телебачення</li>
-                            <li>ось вже:</li>
-                            <div className={'h-[48px] max-w-[282px] max-[1920px]:max-w-[216px] min-w-[216px] rounded-xl bg-[#123853]'}>
-                                <p className={'font-semibold ml-[17px] pt-[7px]'}>24 роки 5 місяців</p>
-                            </div>
+                            <li>Для Бізнесу</li>
+                            <li>Для Багатоповерхівок</li>
+                            <li>Для Приватних Будинків</li>
                         </ul>
                     </div>
-                    
-                </div>
-                <div className={'col-span-1 row-start-3'}>
-                        <a className={'text-[#56AABF] font-semibold leading-[34px]'}>© Batyevka 2014-2024</a>
-                </div>
-                <div>
-                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>ТАРИФИ</h3>
-                    <ul className={'leading-[36px]'}>
-                        <li>Для Бізнесу</li>
-                        <li>Для Багатоповерхівок</li>
-                        <li>Для Приватних Будинків</li>
-                    </ul>
-                </div>
-                <div className={'col-span-1 row-span-2 max-[1280px]:col-start-2 max-[1280px]:row-start-2'}>
-                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Інформація абонентам</h3>
-                    <ul className={'leading-[36px]'}>
-                        <li>Публічний договір</li>
-                        <li>Мапа покриття</li>
-                        <li>Особитстий кабінет</li>
-                        <li>Додаткові опції</li>
-                        <li>Ще щось цікаве</li>
-                    </ul>
-                </div>
-                <div>
-                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Технічна підтримка</h3>
-                    <ul className={'leading-[36px]'}>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[21px] max-[1920px]:w-[16px] max-[1920px]:h-[16px]' src={orange_mini_phone} alt={``}/>044 49 555 49</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[16px] max-[1920px]:w-[16px] max-[1920px]:h-[12px]' src={orange_mini_mail} alt={``}/>support@batyevka.net</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[19px] max-[1920px]:w-[17px] max-[1920px]:h-[15px]' src={orange_mini_tg_logo} alt={``}/>бот в телеграмм</li>
-                    </ul>
-                </div>
-                <div className={'col-span-1 max-[1024px]:col-start-3 max-[1024px]:row-start-2 max-[1024px]:row-span-1'}>
-                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Контакти</h3>
-                    <ul className={'leading-[36px]'}>
-                        <li><Image className='inline mr-[10px] w-[16px] h-[21px] max-[1920px]:w-[12px] max-[1920px]:h-[16px]' src={orange_mini_place_marker} alt={``}/>03110, м. Київ, а/с 26</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[21px] max-[1920px]:w-[16px] max-[1920px]:h-[16px]' src={orange_mini_phone} alt={``}/>0 800 30 32 30</li>
-                        <li><Image className='inline mr-[10px] w-[21px] h-[16px] max-[1920px]:w-[16px] max-[1920px]:h-[12px]' src={orange_mini_mail} alt={``}/>dogovor@batyevka.net</li>
-                    </ul>    
-                </div>
-                <div className={'max-[1920px]:col-start-5 max-[1920px]:row-start-2 max-[1920px]:row-span-1 max-[1280px]:col-start-4 max-[1280px]:row-start-2 max-[1280px]:row-span-1 max-[1024px]:col-start-3 max-[1024px]:row-start-3 max-[1024px]:row-span-1'}>
-                    <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[24px] max-[1920px]:mb-[19px]`}>Соціальні мережі</h3>
-                    <div className={`flex`}>
-                        <a href={``}><Image className='w-[40px] h-[40px] max-[1920px]:w-[30px] max-[1920px]:h-[30px] mr-[8px]' src={_1} alt={``}/></a>
-                        <a href={``}><Image className='w-[40px] h-[40px] max-[1920px]:w-[30px] max-[1920px]:h-[30px]' src={_3} alt={``}/></a>
+                    <div className={'col-span-1 row-span-2 max-[1280px]:col-start-2 max-[1280px]:row-start-2'}>
+                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Інформація абонентам</h3>
+                        <ul className={'leading-[36px]'}>
+                            <li>Публічний договір</li>
+                            <li>Мапа покриття</li>
+                            <li>Особитстий кабінет</li>
+                            <li>Додаткові опції</li>
+                            <li>Ще щось цікаве</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Технічна підтримка</h3>
+                        <ul className={'leading-[36px]'}>
+                            <li><Image className='inline mr-[10px] w-[21px] h-[21px] max-[1920px]:w-[16px] max-[1920px]:h-[16px]' src={orange_mini_phone} alt={``}/>044 49 555 49</li>
+                            <li><Image className='inline mr-[10px] w-[21px] h-[16px] max-[1920px]:w-[16px] max-[1920px]:h-[12px]' src={orange_mini_mail} alt={``}/>support@batyevka.net</li>
+                            <li><Image className='inline mr-[10px] w-[21px] h-[19px] max-[1920px]:w-[17px] max-[1920px]:h-[15px]' src={orange_mini_tg_logo} alt={``}/>бот в телеграмм</li>
+                        </ul>
+                    </div>
+                    <div className={'col-span-1 max-[1024px]:col-start-3 max-[1024px]:row-start-2 max-[1024px]:row-span-1'}>
+                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[8px]`}>Контакти</h3>
+                        <ul className={'leading-[36px]'}>
+                            <li><Image className='inline mr-[10px] w-[16px] h-[21px] max-[1920px]:w-[12px] max-[1920px]:h-[16px]' src={orange_mini_place_marker} alt={``}/>03110, м. Київ, а/с 26</li>
+                            <li><Image className='inline mr-[10px] w-[21px] h-[21px] max-[1920px]:w-[16px] max-[1920px]:h-[16px]' src={orange_mini_phone} alt={``}/>0 800 30 32 30</li>
+                            <li><Image className='inline mr-[10px] w-[21px] h-[16px] max-[1920px]:w-[16px] max-[1920px]:h-[12px]' src={orange_mini_mail} alt={``}/>dogovor@batyevka.net</li>
+                        </ul>    
+                    </div>
+                    <div className={'max-[1920px]:col-start-5 max-[1920px]:row-start-2 max-[1920px]:row-span-1 max-[1280px]:col-start-4 max-[1280px]:row-start-2 max-[1280px]:row-span-1 max-[1024px]:col-start-3 max-[1024px]:row-start-3 max-[1024px]:row-span-1'}>
+                        <h3 className={`border-b border-[#56AABF] border-solid font-semibold pb-[11px] mb-[24px] max-[1920px]:mb-[19px]`}>Соціальні мережі</h3>
+                        <div className={`flex`}>
+                            <a href={``}><Image className='w-[40px] h-[40px] max-[1920px]:w-[30px] max-[1920px]:h-[30px] mr-[8px]' src={_1} alt={``}/></a>
+                            <a href={``}><Image className='w-[40px] h-[40px] max-[1920px]:w-[30px] max-[1920px]:h-[30px]' src={_3} alt={``}/></a>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className={'min-[800px]:hidden'}>
+            <Accordion type="single" collapsible className={'mr-[35px] ml-[35px] text-[14px] pt-[80px]'}>
+            <AccordionItem value="item-1">
+                <AccordionTrigger className="h-[48px] flex items-center w-full">
+                    <h3 className={`border-b border-[#56AABF] border-solid w-full text-white font-medium pb-[11px] mb-[8px] text-left`}>Ми надаємо послуги:</h3>
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col ml-[20px] text-white">
+                        <Link href={'#'}>
+                        Інтернет
+                    </Link>
+                    <Link href={'#'}>
+                        Інтернет + ТВ
+                    </Link>
+                    <Link href={'#'}>
+                        Телебачення
+                    </Link>
+                    <Link href={'#'}>
+                        Комп'ютерна допомога
+                    </Link>
+                    <Link href={'#'}>
+                        Всі опції
+                    </Link>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+                <AccordionTrigger className="h-[48px] flex items-center w-full">
+                    <h3 className={`border-b border-[#56AABF] border-solid w-full text-white font-medium pb-[11px] mb-[8px] text-left`}>ТАРИФИ</h3>
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col ml-[20px] text-white">
+                    <Link href={'#'}>
+                        Інтернет
+                    </Link>
+                    <Link href={'#'}>
+                        Інтернет + ТВ
+                    </Link>
+                    <Link href={'#'}>
+                        Комп'ютерна допомога
+                    </Link>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger className="h-[48px] flex items-center w-full">
+                    <h3 className={` w-full text-white font-medium pb-[11px] mb-[8px] text-left`}>Інформація абонентам</h3>
+                    
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col ml-[20px] text-white">
+                    <Link href={'#'}>
+                        Акції
+                    </Link>
+                    <Link href={'#'}>
+                        Тарифи
+                    </Link>
+                    <Link href={'#'}>
+                        Мапа покриття
+                    </Link>
+                    <Link href={'#'}>
+                        Абоненту
+                    </Link>
+                    <Link href={'#'}>
+                        Всі опції
+                    </Link>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+                <AccordionTrigger className="h-[48px] flex items-center w-full">
+                    <h3 className={`border-b border-[#56AABF] border-solid w-full text-white font-medium pb-[11px] mb-[8px] text-left`}>Технічна підтримка</h3>
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col ml-[20px] text-white">
+                    <Link href={'#'}>
+                        Акції
+                    </Link>
+                    <Link href={'#'}>
+                        Тарифи
+                    </Link>
+                    <Link href={'#'}>
+                        Мапа покриття
+                    </Link>
+                    <Link href={'#'}>
+                        Абоненту
+                    </Link>
+                    <Link href={'#'}>
+                        Всі опції
+                    </Link>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-5">
+                <AccordionTrigger className="h-[48px] flex items-center w-full">
+                    <h3 className={`border-b border-[#56AABF] border-solid w-full text-white font-medium pb-[11px] mb-[8px] text-left`}>Контакти</h3>
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col ml-[20px] text-white">
+                    <Link href={'#'}>
+                        Акції
+                    </Link>
+                    <Link href={'#'}>
+                        Тарифи
+                    </Link>
+                    <Link href={'#'}>
+                        Мапа покриття
+                    </Link>
+                    <Link href={'#'}>
+                        Абоненту
+                    </Link>
+                    <Link href={'#'}>
+                        Всі опції
+                    </Link>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-6">
+                <AccordionTrigger className="h-[48px] flex items-center w-full">
+                    <h3 className={`border-b border-[#56AABF] border-solid w-full text-white font-medium pb-[11px] mb-[8px] text-left`}>Соціальні мережі</h3>
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col ml-[20px] text-white">
+                    <Link href={'#'}>
+                        Акції
+                    </Link>
+                    <Link href={'#'}>
+                        Тарифи
+                    </Link>
+                    <Link href={'#'}>
+                        Мапа покриття
+                    </Link>
+                    <Link href={'#'}>
+                        Абоненту
+                    </Link>
+                    <Link href={'#'}>
+                        Всі опції
+                    </Link>
+                </AccordionContent>
+            </AccordionItem>
+            </Accordion>
             </div>
         </footer>
      );
