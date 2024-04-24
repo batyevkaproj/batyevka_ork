1c41c4cbc8867adbbb903a27fb1875a463e8e1c7
Kolyaska2002 <artsem112@gmail.com>
2024-04-25 01:02:36 +0200

    tarifs for smaller screens finished(need to add margins to the bigger res) FIX THE HEADING

--- components/business-page/Tarifs.tsx
+++ components/business-page/Tarifs.tsx
@@ -260,7 +260,7 @@ const Tarifs = () => {
                             </div>
                         </CarouselItem>
                         <CarouselItem key={3} className="basis-1/2 flex justify-between items-center ">
-                            <div className="p-2">
+                            <div className="p-1">
                                 <div className="flex justify-center items-center pt-[20px] mb-[20px]">
                                     <div className="h-[573px] w-[350px] rounded-md bg-[#123853]">
                                         <h2 className="text-[#DC662D] font-bold text-[70px] justify-center flex mt-[60px] leading-[70px]">
@@ -544,15 +544,153 @@ const Tarifs = () => {
                             <WhiteGreyIpCheck />
                         </div>
                         <div className="flex justify-center">
-                            <button className="bg-[#56AABF] text-white rounded-full h-[60px] w-[155px] cursor-pointer  font-semibold shadow-[0_4px_20px_0_#56AABF80]">Прдключення</button>
+                            <button className="bg-[#56AABF] text-white rounded-full h-[60px] w-[155px] cursor-pointer font-semibold shadow-[0_4px_20px_0_#56AABF80]">Прдключення</button>
                         </div>
                     </div>
                 </div>
             </div>
 
             {/*XG-GPON middle */}
-            <div className="min-[1248px]:hidden max-[558px]:hidden">
-             
+            <div className="min-[1248px]:hidden max-[558px]:hidden pt-[40px]">
+            <Heading text={`Бізнес тарифи "XG-PON"`} text_size={42} />
+                <div className="my-[60px] mr-[68px]">
+                    <OpticCable/>
+                </div>
+                <div className="text-[26px] leading-[32px] font-bold text-white flex items-center justify-center">
+                    <h1 className="text-center w-[913px] flex items-center justify-center">Підключаємо виділеним ОПТИЧНИМ кабелем із точки обміну трафіком</h1>
+                </div>
+                <div className="text-[24px] leading-[36px] font-bold text-white flex justify-center pt-[18px]">
+                    <h1 className="text-center text-[24px] align-bottom">Всього за<span className=" text-[#56AABF] px-[10px] text-[42px]">14 999</span>грн</h1>
+                </div>
+
+                <div className="flex justify-center box-border pt-[40px] mx-[35px]">
+                <Carousel className="w-[760px]" opts={{ loop: false, slidesToScroll : 2 }}>
+                    <CarouselContent className="-ml-1" >
+                        <CarouselItem key={1} className="basis-1/2 flex justify-between items-center ">
+                            <div className="p-1">
+                                <div className="flex justify-center items-center pt-[20px] mb-[20px]">
+                                    <div className="h-[573px] w-[350px] rounded-md bg-[#123853]">
+                                        <h2 className="text-[#DC662D]  font-bold text-[70px] justify-center flex mt-[60px] leading-[70px]">
+                                            1 299
+                                        </h2>
+                                        <h2 className="text-[#DC662D] font-normal text-[20px] leading-[22px] justify-center flex pb-[77px]">
+                                            грн/міс
+                                        </h2>
+
+                                        <h2 className="text-[#56AABF] font-semibold text-[18px] leading-[22px] justify-center flex pb-[10px]">
+                                            Швидкість
+                                        </h2>
+                                        <h2 className="text-white  font-bold text-[36px] leading-[36px] justify-center flex pb-[8px]">
+                                            2000 Мегабіт
+                                        </h2>
+                                        <div className="flex justify-center text-white text-[18px]">
+                                            <span className="flex mr-[20px] ">
+                                                <Image src={galochka} alt="galochka" className="mr-2 " />
+                                                УКРАЇНА
+                                            </span>
+                                            <span className="flex ml-1.5">
+                                                <Image src={galochka} alt="galochka" className="mr-2" />
+                                                СВІТ
+                                            </span>
+                                        </div>
+                                        <h2 className="text-[#56AABF]  font-semibold text-[18px] justify-center flex pb-[40px]">
+                                            рівнозначна
+                                        </h2>
+                                        <div className="flex justify-center mb-[30px]">
+                                            <WhiteGreyIpCheck />
+                                        </div>
+                                        <div className="flex justify-center">
+                                            <button className="bg-[#56AABF] text-white rounded-full h-[60px] w-[270px] mx-[32px] cursor-pointer  font-semibold shadow-[0_4px_20px_0_#56AABF80]">Підключити</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                        
+                        </CarouselItem>
+                        <CarouselItem key={2} className="basis-1/2 flex justify-between items-center">
+                            <div className="p-1">
+                                <div className="flex justify-center items-center pt-[20px] mb-[20px]">
+                                    <div className="h-[573px] w-[350px] rounded-md bg-[#123853]">
+                                        <h2 className="text-[#DC662D]  font-bold text-[70px] justify-center flex mt-[60px] leading-[70px]">
+                                            1 699
+                                        </h2>
+                                        <h2 className="text-[#DC662D] font-normal text-[20px] leading-[22px] justify-center flex pb-[77px]">
+                                            грн/міс
+                                        </h2>
+
+                                        <h2 className="text-[#56AABF] font-semibold text-[18px] leading-[22px] justify-center flex pb-[10px]">
+                                            Швидкість
+                                        </h2>
+                                        <h2 className="text-white  font-bold text-[36px] leading-[36px] justify-center flex pb-[8px]">
+                                            2000 Мегабіт
+                                        </h2>
+                                        <div className="flex justify-center text-white text-[18px]">
+                                            <span className="flex mr-[20px] ">
+                                                <Image src={galochka} alt="galochka" className="mr-2 " />
+                                                УКРАЇНА
+                                            </span>
+                                            <span className="flex ml-1.5">
+                                                <Image src={galochka} alt="galochka" className="mr-2" />
+                                                СВІТ
+                                            </span>
+                                        </div>
+                                        <h2 className="text-[#56AABF]  font-semibold text-[18px] justify-center flex pb-[40px]">
+                                            рівнозначна
+                                        </h2>
+                                        <div className="flex justify-center mb-[30px]">
+                                            <WhiteGreyIpCheck />
+                                        </div>
+                                        <div className="flex justify-center">
+                                            <button className="bg-[#56AABF] text-white rounded-full h-[60px] w-[270px] mx-[32px] cursor-pointer font-semibold shadow-[0_4px_20px_0_#56AABF80]">Підключити</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem key={3} className="basis-1/2 flex justify-between items-center ">
+                            <div className="p-2">
+                                <div className="flex justify-center items-center pt-[20px] mb-[20px]">
+                                    <div className="h-[573px] w-[350px] rounded-md bg-[#123853]">
+                                        <h2 className="text-[#DC662D]  font-bold text-[70px] justify-center flex mt-[60px] leading-[70px]">
+                                            3 999
+                                        </h2>
+                                        <h2 className="text-[#DC662D] font-normal text-[20px] leading-[22px] justify-center flex pb-[77px]">
+                                            грн/міс
+                                        </h2>
+
+                                        <h2 className="text-[#56AABF] font-semibold text-[18px] leading-[22px] justify-center flex pb-[10px]">
+                                            Швидкість
+                                        </h2>
+                                        <h2 className="text-white  font-bold text-[36px] leading-[36px] justify-center flex pb-[8px]">
+                                            2000 Мегабіт
+                                        </h2>
+                                        <div className="flex justify-center text-white text-[18px]">
+                                            <span className="flex mr-[20px] ">
+                                                <Image src={galochka} alt="galochka" className="mr-2 " />
+                                                УКРАЇНА
+                                            </span>
+                                            <span className="flex ml-1.5">
+                                                <Image src={galochka} alt="galochka" className="mr-2" />
+                                                СВІТ
+                                            </span>
+                                        </div>
+                                        <h2 className="text-[#56AABF]  font-semibold text-[18px] justify-center flex pb-[40px]">
+                                            рівнозначна
+                                        </h2>
+                                        <div className="flex justify-center mb-[30px]">
+                                            <WhiteGreyIpCheck />
+                                        </div>
+                                        <div className="flex justify-center">
+                                            <button className="bg-[#56AABF] text-white rounded-full h-[60px] w-[270px] mx-[32px] cursor-pointer  font-semibold shadow-[0_4px_20px_0_#56AABF80]">Підключити</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious className="ml-[32px]"/>
+                    <CarouselNext className="mr-[32px]"/>
+                </Carousel>
+                </div>
             </div>
 
             {/* XG-GPON mobile */}
@@ -679,7 +817,7 @@ const Tarifs = () => {
             </div>
 
             {/* */}
-            <div className={'max-[558px]:hidden mr-[170px] ml-[170px] max-[2377px]:mr-[120px] max-[2377px]:ml-[120px] max-[1600px]:mr-[85px] max-[1600px]:ml-[85px] max-[1247px]:mr-[67px] max-[1247px]:ml-[67px] max-[932px]:mr-[35px] max-[932px]:ml-[35px] min-[3644px]:mr-[240px] min-[3644px]:ml-[240px]'}>
+            <div className={'max-[558px]:hidden mr-[170px] ml-[170px] max-[2377px]:mr-[120px] max-[2377px]:ml-[120px] max-[1600px]:mr-[85px] max-[1600px]:ml-[85px] max-[1247px]:mr-[67px] max-[1247px]:ml-[67px] max-[932px]:mr-[35px] max-[932px]:ml-[35px] min-[3644px]:mr-[240px] min-[3644px]:ml-[240px] min-[3644px]:pt-[180px] pt-[120px] max-[2377px]:pt-[90px] max-[1247px]:pt-[42px] max-[932px]:pt-[12px]'}>
                 <Heading text="Механіка тарифів та розрахунків" text_size={42}/>
                 <div className="min-[2377px]:grid min-[2377px]:grid-cols-2 gap-[40px] min-[3644px]:gap-[60px] font-normal text-white flex flex-col items-center justify-center mt-[52px] max-[2377px]:mt-[40px] min-[3644px]:mt-[78px] mb-[120px] max-[2377px]:leading-[24px] max-[2377px]:text-[16px] leading-[28px] text-[20px] min-[3644px]:leading-[42px] min-[3644px]:text-[30px]">
                     <div className="col-span-1 max-[2377px]:mb-[-38px]">
