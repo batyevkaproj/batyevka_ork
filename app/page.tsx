35801f67bfc993e48050401f8b99c81a0c4d14fe
Ihor Radetskyi <i.radetskyi@batyevka.net>
2024-04-25 00:03:31 +0300

    messing with git

--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,6 @@ import OpticCable from '@/components/business-page/OpticCable';
 
 
 export default function Home() {
-
-  const [lorems, setComments] = useState([])
-  const [lorem, setComment] = useState("lorem")
-  //https://www.youtube.com/watch?v=wqHGLjuXuHo
-  // const res = await fetch('https://api.api-ninjas.com/v1/loremipsum?paragraphs=1')
-  //https://medium.com/@a.pirus/create-a-modal-that-can-be-opened-from-anywhere-in-next-js-13-36f6d0ce1bcf
-  const fetchComments = async () => {
-    const response = await fetch('https://api.api-ninjas.com/v1/loremipsum?paragraphs=1')
-    const data = await response.json()
-    setComment(data)
-  }
   return (
     <div className={'min-w-[350px]'}>
       <Header />
