c4bb7a7b7ec7cb4c97db7a748be9760bdef37d7a
Ihor Chyshkala <ihor@chyshkala.com>
2024-09-28 20:55:03 +0100

    Refactoring WIP II

--- components/business-page/CalculatorTarifs.tsx
+++ components/business-page/CalculatorTarifs.tsx
@@ -4,9 +4,11 @@ import { TarifsSwitch, RegularSwitch } from "../ui/switches";
 import { Checkbox } from "@/components/ui/checkbox_calculator"
 import { useLocalStorage } from 'usehooks-ts'
 
+import type { TVinfoItem } from "../tariff-page/TVBlock";
 
-import { TVinfo } from "@/constants/megogo";
+// import { TVinfo } from "@/constants/megogo";
 import InternetBlock from "@/components/tariff-page/InternetBlock";
+import TVBlock from "../tariff-page/TVBlock";
 
 
 const CalculatorTarifs = ({ theme }: any) => {
@@ -20,7 +22,7 @@ const CalculatorTarifs = ({ theme }: any) => {
     const [tvBundle, setTvBundle, removeTvBundle] = useLocalStorage('tvBundle', 0)
 
 
-    const TVinfo = [
+    const TVinfo: TVinfoItem[] = [
         { id: 0, name: '?', channels: '0', movies: '0', show: false, button: 'Promo 14 днів', string1: '0 каналiв для ', string2: 'любителів ТБ, а також ', string3: 'колекцiя фiльмiв, ', string4: 'мультикiв та серiалiв', from_col: 'from-[#3E3D39]', to_col: 'to-[#1B211F]', button_col: 'bg-[#303030]' },
         { id: 1, name: 'Легка', channels: '298', movies: '10000+', show: true, button: 'Promo 14 днів', string1: '298 каналiв для ', string2: 'любителів ТБ, а також ', string3: 'колекцiя фiльмiв, ', string4: 'мультикiв та серiалiв', from_col: 'from-[#3E3D39]', to_col: 'to-[#1B211F]', button_col: 'bg-[#303030]' },
         { id: 2, name: 'Оптимальна', channels: '389', movies: '11400+', show: true, button: 'Спробувати за 1 грн*', string1: '389 каналiв i фiльми, ', string2: 'серiали, мультики топових ', string3: 'кiностудiй для поцiновувачiв ', string4: 'кiно', from_col: 'from-[#333A4C]', to_col: 'to-[#25283B]', button_col: 'bg-[#323A4B]' },
@@ -36,7 +38,6 @@ const CalculatorTarifs = ({ theme }: any) => {
         } else {
             setTvBundle(0);
         }
-
     }
 
     return (
@@ -140,36 +141,13 @@ const CalculatorTarifs = ({ theme }: any) => {
                     <div className={``}>
                         <div className={`col-span-1 col-start-2 max-[1800px]:col-start-1 flex justify-center min-[3644px]:h-[1272px] h-[848px] max-[2377px]:h-[648px] min-[3644px]:gap-[12px] gap-[10px] max-[2377px]:gap-[8px] max-[780px]:hidden`}>
                             
-                            <InternetBlock />
+                            <InternetBlock isTarifsSwitch={isTarifsSwitch} speedUtp={speedUtp} speedGpon={speedGpon}/>
                             <div className={`${TVinfo[tvBundle].show ? '' : 'opacity-[0.4]'} min-[3644px]:text-[120px] min-[3644px]:leading-[120px] text-[80px] leading-[80px] max-[2377px]:text-[60px] max-[2377px]:leading-[60px] font-bold text-[#5F6061] flex items-center`}>
                                 <p>+</p>
                             </div>
-                            <div className={`${TVinfo[tvBundle].show ? '' : 'opacity-[0.4]'} text-white min-[3644px]:w-[687px] w-[458px] max-[2377px]:w-[350px] max-[880px]:w-[320px] shadow-[0_4px_29px_0px_#E6E3E3] rounded-[10px] bg-gradient-to-r + ${TVinfo[tvBundle].from_col} + ${TVinfo[tvBundle].to_col} min-w-[320px]`}>
-                                <p className={`flex items-center justify-center font-bold min-[3644px]:text-[48px] min-[3644px]:leading-[60px] text-[32px] leading-[40px] max-[2377px]:text-[24px] max-[2377px]:leading-[30px] min-[3644px]:pt-[78px] pt-[52px] max-[2377px]:pt-[40px]`}>Телебачення</p>
-                                <div className={`flex items-center justify-center`}>
-                                    <p className={`flex items-center justify-center min-[3644px]:mt-[90px] mt-[62px] max-[2377px]:mt-[48px] rounded-[4px] bg-[#FD363B] min-[3644px]:w-[132px] min-[3644px]:h-[51px] w-[88px] h-[34px] max-[2377px]:w-[67px] max-[2377px]:h-[26px] font-bold min-[3644px]:text-[24px] min-[3644px]:leading-[24px] text-[16px] leading-[16px] max-[2377px]:text-[13px] max-[2377px]:leading-[13px]`}>АКЦIЯ</p>
-                                </div>
-                                <p className={`flex items-center justify-center font-bold min-[3644px]:text-[42px] min-[3644px]:leading-[42px] text-[28px] leading-[28px] max-[2377px]:text-[22px] max-[2377px]:leading-[22px] min-[3644px]:pt-[90px] pt-[65px] max-[2377px]:pt-[50px]`}>{TVinfo[tvBundle].name}</p>
-                                <div className={`flex items-center justify-center text-center font-bold text-[#909090] min-[3644px]:mt-[22px] mt-[16px] max-[2377px]:mt-[12px] min-[3644px]:text-[30px] min-[3644px]:leading-[42px] text-[20px] leading-[28px] max-[2377px]:text-[16px] max-[2377px]:leading-[22px]`}>
-                                    {TVinfo[tvBundle].string1} <br /> {TVinfo[tvBundle].string2}<br />{TVinfo[tvBundle].string3}<br />{TVinfo[tvBundle].string4}
-                                </div>
-                                <div className={'flex items-center justify-center min-[3644px]:gap-[143px] gap-[89px] max-[2377px]:gap-[49px] font-bold min-[3644px]:mt-[76px] mt-[51px] max-[2377px]:mt-[39px]'}>
-                                    <div className={``}>
-                                        <p className={`min-[3644px]:text-[72px] min-[3644px]:leading-[72px] text-[48px] leading-[48px] max-[2377px]:text-[36px] max-[2377px]:leading-[36px] flex justify-center`}>{TVinfo[tvBundle].channels}</p>
-                                        <p className={`min-[3644px]:text-[30px] min-[3644px]:leading-[30px] text-[20px] leading-[20px] max-[2377px]:text-[16px] max-[2377px]:leading-[13px] text-[#909090] pt-[9px] flex justify-center`}>каналiв</p>
-                                    </div>
-                                    <div className={``}>
-                                        <p className={`min-[3644px]:text-[72px] min-[3644px]:leading-[72px] text-[48px] leading-[48px] max-[2377px]:text-[36px] max-[2377px]:leading-[36px] flex justify-center`}>{TVinfo[tvBundle].movies}</p>
-                                        <p className={`min-[3644px]:text-[30px] min-[3644px]:leading-[30px] text-[20px] leading-[20px] max-[2377px]:text-[16px] max-[2377px]:leading-[13px] text-[#909090] pt-[9px] flex justify-center`}>фiльмiв</p>
-                                    </div>
-                                </div>
-                                <p className={`font-bold min-[3644px]:text-[30px] min-[3644px]:leading-[42px] text-[20px] leading-[28px] text-[16px] leading-[22px] flex items-center justify-center text-center min-[3644px]:mt-[76px] mt-[51px] max-[2377px]:mt-[39px]`}>Перемотка i ТБ-архiв на <br />каналах</p>
-                                <div className={`flex items-center justify-center min-[3644px]:w-[519px] min-[3644px]:h-[118px] w-[346px] h-[78px] max-[2377px]:w-[264px] max-[2377px]:h-[60px] border-white border-[1px] rounded-[7px] min-[3644px]:ml-[84px] ml-[56px] max-[2377px]:ml-[43px] max-[880px]:ml-[28px] min-[3644px]:mt-[118px] mt-[76px] max-[2377px]:mt-[51px] + ${TVinfo[tvBundle].button_col}`}>
-                                    <p className={` min-[3644px]:text-[30px] min-[3644px]:leading-[42px] text-[20px] leading-[28px] max-[2377px]:text-[16px] max-[2377px]:leading-[22px] font-bold`}>{TVinfo[tvBundle].button} </p>
-                                </div>
-                            </div>
+                            <TVBlock TVinfo={TVinfo} tvBundle={tvBundle}/>
                         </div>
-                        <div className={`flex justify-center`}>
+                        <div className="flex justify-center">
                             <div className={`min-[3644px]:mt-[117px] mt-[78px] max-[2377px]:mt-[60px] max-[680px]:mt-[-30px] min-[3664px]:mr-[117px] mr-[78px] max-[2377px]:mr-[60px] w-full max-[1800px]:w-[750px] max-[1800px]:mr-[20px] max-[1800px]:ml-[20px]`}>
                                 <div className={`flex grid grid-cols-1 items-center w-full font-bold min-[3644px]:text-[36px] min-[3644px]:leading-[42px] text-[24px] leading-[28px] max-[2377px]:text-[18px] max-[2377px]:leading-[22px] min-[3644px]:gap-[22px] gap-[15px] max-[2377px]:gap-[12px] max-[680px]:hidden`}>
                                     <div className={`flex items-end justify-between border-b-[2px] border-[#F4F2F2] border-solid min-[3644px]:pb-[20px] pb-[13px] max-[2377px]:pb-[10px]`}>
