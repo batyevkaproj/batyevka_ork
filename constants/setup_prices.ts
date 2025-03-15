a9e567c5313730044f05fb93c964e2827e4a1e7c
Ihor Chyshkala <ihor@chyshkala.com>
2025-03-15 15:58:37 +0000

    WIP

--- constants/setup_prices.ts
+++ constants/setup_prices.ts
@@ -4,19 +4,18 @@ export type SetupPriceProps = {
     default?: boolean;
 }
 
-
 export const UTP_SETUP_PRICES: SetupPriceProps[] = [
-    { months: 1, price: 499, default: true },
-    { months: 6, price: 199 },
+    { months: 1, price: 499 },
+    { months: 6, price: 199, default: true },
     { months: 12, price: 1 },
     { months: 24, price: 1 },
     { months: 36, price: 1 }
 ];
 
 export const GPON_SETUP_PRICES: SetupPriceProps[] = [
-    { months: 1, price: 1499, default: true },
-    { months: 6, price: 999 },
-    { months: 12, price: 499 },
-    { months: 24, price: 100 },
+    { months: 1, price: 1499 },
+    { months: 6, price: 1, default: true },
+    { months: 12, price: 1 },
+    { months: 24, price: 1 },
     { months: 36, price: 1 }
 ];
