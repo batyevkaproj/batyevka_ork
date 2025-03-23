9399e9969e8d805a67c2d7cc5d82c2bd0708ea1e
keyman <keyman@bztest1.batyevka.net>
2025-03-23 14:51:31 +0200

    git is hard

--- constants/internet_speeds.ts
+++ constants/internet_speeds.ts
@@ -6,7 +6,7 @@ export type SpeedItemProps = {
 }
 
 export const GPON_SPEEDS: SpeedItemProps[] = [
-    { value: 1, speed: 300, measure: 'Мбіт', price: 199 },
+    { value: 1, speed: 300, measure: 'Мбіт', price: 200 },
     { value: 2, speed: 1, measure: 'Гбіт', price: 250 },
     { value: 3, speed: 2.5, measure: 'Гбіт', price: 1120 },
     { value: 4, speed: 5, measure: 'Гбіт', price: 2245 }
