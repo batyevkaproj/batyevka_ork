da15d303dbc212f5d29ef6935c72c16ef3a48b56
root <root@bztest1.batyevka.net>
2025-06-17 22:39:56 +0300

    montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Montserrat } from 'next/font/google';
 import BinotelScripts from "@/components/BinotelScripts";
 
 const montserrat = Montserrat({
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   display:'swap',
   fallback: ['Arial', 'sans-serif'],
